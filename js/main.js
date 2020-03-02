18de02acbb2c3d0f067784d8007ff699bca1251e
Denis <uzor147147@mail.ru>
2020-03-02 18:30:31 +0300

    2.03

--- js/main.js
+++ js/main.js
@@ -1,15 +1,16 @@
 'use strict';
 
-var COMMENTS = [
-  'Всё отлично!',
-  'В целом всё неплохо. Но не всё.',
-  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
-  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
-  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
-  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
+
+var MESSAGES = [
+'Всё отлично!',
+'В целом всё неплохо. Но не всё.',
+'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
+'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
 ];
 
-var userNames = [
+var USER_NAMES = [
   'Cергей',
   'Федя',
   'Василий',
@@ -18,45 +19,45 @@ var userNames = [
   'Анастасия'
 ];
 
-var avatarNumber = 6;
-var commentsMin = 0;
-var commentsMax = 30;
-var userPosts = 25;
-var minLikes = 15;
-var maxLikes = 200;
+var COMMENTS_LIMIT = 6;
+var COMMENTS_COUNT_MIN = 0;
+var COMMENTS_COUNT_MAX = 50;
+var USER_PHOTOS_LIMIT = 25;
+var LIKES_COUNT_MIN = 15;
+var LIKES_COUNT_MAX = 200;
 
-var randomNumber = function (from, to) {
+var generateRandomNumber = function (from, to) {
   return Math.round(Math.random() * (to - from) + from);
 };
 
 var getRandomItem = function (items) {
-  return items[randomNumber(0, items.length - 1)];
+  return items[generateRandomNumber(0, items.length - 1)];
 };
 
-var randomComment = function () {
+var generateRandomComment = function () {
   return {
-    avatar: 'img/avatar-' + randomNumber(1, 6) + '.svg',
-    message: getRandomItem(COMMENTS),
-    name: getRandomItem(userNames)
+    avatar: 'img/avatar-' + generateRandomNumber(1, 6) + '.svg',
+    message: getRandomItem(MESSAGES),
+    name: getRandomItem(USER_NAMES)
   };
 };
 
-var randomComments = function () {
+var generateRandomComments = function () {
   var comments = [];
-  for (var i = 0; i < avatarNumber; i++) {
-    comments.push(randomComment());
+  for (var i = 0; i < COMMENTS_LIMIT; i++) {
+    comments.push(generateRandomComment());
   }
   return comments;
 };
 
 var generateRandomUserPhotos = function () {
   var randomUserPhotos = [];
-  for (var i = 0; i < userPosts; i++) {
+  for (var i = 0; i < USER_PHOTOS_LIMIT; i++) {
     randomUserPhotos.push({
       url: 'photos/' + (i + 1) + '.jpg',
       desctiption: 'Описание фотографии',
-      likes: randomNumber(minLikes, maxLikes),
-      comments: randomComments()
+      likes: generateRandomNumber(LIKES_COUNT_MIN, LIKES_COUNT_MAX),
+      comments: generateRandomComments()
     });
   }
   return randomUserPhotos;
@@ -66,7 +67,7 @@ var createUserPhotoElement = function (userPhoto) {
   var userPhotoElement = userPhotoTemplateElement.cloneNode(true);
 
   userPhotoElement.querySelector('.picture__img').src = userPhoto.url;
-  userPhotoElement.querySelector('.picture__comments').textContent = userPhoto.comments.length + randomNumber(commentsMin, commentsMax);
+  userPhotoElement.querySelector('.picture__comments').textContent = userPhoto.comments.length + generateRandomNumber(COMMENTS_COUNT_MIN, COMMENTS_COUNT_MAX);
   userPhotoElement.querySelector('.picture__likes').textContent = userPhoto.likes;
 
   return userPhotoElement;
